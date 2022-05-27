014fce5e4789b7b8ceb386fe8d262acf4312d7f3
Sohom829 <94981761+Sohom829@users.noreply.github.com>
2022-05-27 04:43:47 +0000

    Input processing and output

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a number"); 
+    int num1;
+    int num2;
+
+     num1 = input.nextInt();
+
+
+
+    System.out.println("Enter another number");
+
+     num2 = input.nextInt();
+
+if(num1 < num2 ) {
+      System.out.println("Num 1 must be greater than number 2");
+    }
+
+    else {
+      double result = num1 - num2;
+      System.out.println("Ans is = " + result);
+    }
   }
 }
